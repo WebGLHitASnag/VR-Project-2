c14046b3bd3bfacd4a18daf23de3365087c9f16c
JakobDozier <35703840+JakobDozier@users.noreply.github.com>
2018-03-29 12:15:59 -0500

    Updated teleport script and script for underwater looks

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour
+{
+
+    public GameObject TargetCube = null;
+
+    public GameObject Player = null;
+
+    private bool startTeleport = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position = TargetCube.transform.position;
+    }
+}
