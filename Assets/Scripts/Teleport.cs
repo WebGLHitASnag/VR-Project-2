4f403eb3432286dc04fec3933a03e664b3ea359b
JakobDozier <35703840+JakobDozier@users.noreply.github.com>
2018-04-05 18:56:12 -0500

    Add files via upload
    (script) final script for teleporting the player

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,24 +1,20 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
 
     public GameObject TargetCube = null;
-
     public GameObject Player = null;
-
-    private bool startTeleport = false;
 
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 
-    void OnTriggerEnter(Collider other)
-    {
+    void OnTriggerEnter(Collider other) {
         Player.transform.position = TargetCube.transform.position;
     }
 }
