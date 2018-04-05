49398cdbadf184503b067fb3a0aff3895eae03fd
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-04-05 00:20:45 -0500

    Added script to link this repository in the menu
    Added more menu assets
    
    Successfully using UnityEngine.SceneManagement

---
+++ Assets/Menu/OpenGitHub.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGitHub : MonoBehaviour {
+
+
+    // When the GitHub logo is clicked on the menu, open our repository
+    public void OpenRepository() {
+        Application.OpenURL("https://github.com/WebGLHitASnag/VR-Project-2");
+    }
+}
