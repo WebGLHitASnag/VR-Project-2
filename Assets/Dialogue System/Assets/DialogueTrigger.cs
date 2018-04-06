b804ac8ef62687251acaccceb846f5a946488f86
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-04-06 00:10:27 -0500

    Merged dialogue system into level 1 from carolynwu@8ec00eee662ab933c2de6817c64e0f80de55f253

--- Assets/Dialogue System/Assets/DialogueTrigger.cs
+++ Assets/Dialogue System/Assets/DialogueTrigger.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour {
 
 	public Dialogue dialogue;
-
+    public GameObject button;
 	public void TriggerDialogue ()
 	{
-		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-	}
+        Debug.Log("WTF");
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        GameObject.Destroy(GameObject.FindGameObjectsWithTag("StartButton")[0]);
+    }
 
 }
