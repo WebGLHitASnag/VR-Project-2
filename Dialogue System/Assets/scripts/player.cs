6a2e35b52595c51a2389683a1d291f7f1ff4387f
carolynwu <wuxujia199438@gmail.com>
2018-04-06 09:06:21 -0500

    add three more animations in the character
    connect the animation

---
+++ Dialogue System/Assets/scripts/player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// link animator to controller
+
+public class player : MonoBehaviour {
+
+    public Animator anim;
+
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("1"))
+        {
+            anim.Play("WAIT01", -1, 0f);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            anim.Play("WAIT02", -1, 0f);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            anim.Play("WAIT03", -1, 0f);
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            anim.Play("WAIT01", -1, 0f);
+        }
+    }
+}
