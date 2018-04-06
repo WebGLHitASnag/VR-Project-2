b236bebde4688239ae30645ccce374f81f239c88
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-04-05 19:39:37 -0500

    Added new and improved dialogue system from https://github.com/WebGLHitASnag/VR-Project-2/commit/8ec00eee662ab933c2de6817c64e0f80de55f253

---
+++ Assets/Dialogue System/Assets/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue {
+
+	public string name;
+
+
+    // the first one is minimual amount of the lines, the second one will be max
+	[TextArea(1, 500)]
+	public string[] sentences;
+
+}
