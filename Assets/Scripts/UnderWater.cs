6fa3faa3e1535320a16f88e50b73d9ca38f8f16d
JakobDozier <35703840+JakobDozier@users.noreply.github.com>
2018-04-08 10:54:36 -0500

    updated underwater
    fixed the height

--- Assets/Scripts/UnderWater.cs
+++ Assets/Scripts/UnderWater.cs
@@ -18,8 +18,8 @@ public class UnderWater : MonoBehaviour {
 	
 	void Update () {
         if (transform.position.x >= 4663.53 && transform.position.x <= 5061.121 && transform.position.z >= -729.498 && transform.position.z <= -331.1509) {
-            if ((transform.position.y <= lakeWater.transform.position.y) != isUnderWater) {
-                isUnderWater = transform.position.y < lakeWater.transform.position.y;
+            if ((transform.position.y <= lakeWater.transform.position.y - 1) != isUnderWater) {
+                isUnderWater = transform.position.y < lakeWater.transform.position.y - 1;
                 if (isUnderWater) {
                     setUnderWater();
                 }
@@ -30,8 +30,8 @@ public class UnderWater : MonoBehaviour {
         }
         
         if (transform.position.x >= 345.55 && transform.position.x <= 638.3987 && transform.position.z >= -617.4688 && transform.position.z <= -320.8628) {
-            if ((transform.position.y <= riverWater.transform.position.y) != isUnderWater) {
-                isUnderWater = transform.position.y < riverWater.transform.position.y;
+            if ((transform.position.y <= riverWater.transform.position.y - 1) != isUnderWater) {
+                isUnderWater = transform.position.y < riverWater.transform.position.y - 1;
                 if (isUnderWater) {
                     setUnderWater();
                 }
@@ -43,7 +43,7 @@ public class UnderWater : MonoBehaviour {
         
         if (transform.position.x >= 58.27055 && transform.position.x <= 655.674 && transform.position.z >= 3856.067 && transform.position.z <= 4453.523) {
             if ((transform.position.y <= oceanWater.transform.position.y - 1) != isUnderWater) {
-                isUnderWater = transform.position.y < oceanWater.transform.position.y;
+                isUnderWater = transform.position.y < oceanWater.transform.position.y - 1;
                 if (isUnderWater) {
                     setUnderWater();
                 }
