ce88d17c0ea8b2e601d7194da1b3b05fffa1741f
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-04-09 04:13:17 -0500

    Added level 2
    Added boat scene
    
    Update level 2 button on menu
    
    Added scripts for level 2

---
+++ Assets/Level 1/boat and first person/Assets/scripts/boat.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boat : MonoBehaviour {
+
+	public float turnSpeed = 5000f;
+	public float accellerateSpeed = 5000f;
+
+	private Rigidbody rbody;
+
+	// Use this for initialization
+    //have access to the boats rigidbody
+	void Start () 
+	{
+		rbody = GetComponent<Rigidbody>();
+	}
+	
+	// get the input we press
+	void Update () 
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		rbody.AddTorque(0f,h*turnSpeed*Time.deltaTime,0f);
+        //push the direction which we are facing
+		rbody.AddForce(transform.forward*v*accellerateSpeed*Time.deltaTime);
+
+
+	}
+}
