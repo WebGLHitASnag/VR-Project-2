1a257fc151b9f179ea99cd8361c995be94f82bb5
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-04-09 04:46:10 -0500

    Added controls to menu and build

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -7,18 +7,24 @@ public class MainMenu : MonoBehaviour {
 
     // This function will load a new scene (Level 1)
     public void LoadLevel1() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
     }
 
     // This function will load a new scene (Level 2)
     public void LoadLevel2()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); // Super SIMPLE!
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4); // Super SIMPLE!
     }
 
     // This function will load a new scene (Leaderboard)
-    public void LoadleaderBoard() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    public void LoadLeaderBoard() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // This function will load a new scene (Controls)
+    public void LoadControls()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
 }
