1dd9eab4a5ccba97438fbe6b539986aa297b7963
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-04-10 02:25:15 -0500

    Added scripts to start level 2 from level 1

---
+++ Assets/Level 1/TriggerLevel2Start.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Used to manage scenes
+
+public class TriggerLevel2Start : MonoBehaviour {
+
+    public GameObject boat;
+    float exit_position = -250;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((DialogueManager.continue_count >= 28) && (boat.GetComponent<Transform>().transform.position.x < exit_position)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Load Level 2
+        }
+	}
+}
